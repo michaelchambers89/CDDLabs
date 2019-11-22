a7f572a153c03a4dac9ab7547452aa929047e0c7
mikeycha <32110940+mikeycha@users.noreply.github.com>
2019-11-22 15:31:30 +0000

    lab2

---
+++ ExerciseTwo_Semaphores/html/search/all_0.js
@@ -0,0 +1,5 @@
+var searchData=
+[
+  ['main',['main',['../main_8cpp.html#a840291bc02cba5474a4cb46a9b9566fe',1,'main(void):&#160;main.cpp'],['../signal_8cpp.html#a840291bc02cba5474a4cb46a9b9566fe',1,'main(void):&#160;signal.cpp']]],
+  ['main_2ecpp',['main.cpp',['../main_8cpp.html',1,'']]]
+];
---
+++ ExerciseTwo_Semaphores/main.cpp
@@ -0,0 +1,48 @@
+/*
+ *Project: CDD Labs
+ *Author: Michael Chambers
+ *License:https://creativecommons.org/licenses/by/4.0/legalcode
+ *Date: 22/11/2019
+ */
+#include "Semaphore.h"
+#include <iostream>
+#include <thread>
+#include <unistd.h>
+/*! \class Signal
+    \brief An Implementation of Threads Using Semaphores 
+
+   Uses C++11 features such as mutex and condition variables to implement Semaphores in thread functions 
+
+*/
+/*! displays a message first*/
+void taskOne(std::shared_ptr<Semaphore> theSemaphore, int delay){
+  sleep(delay);
+  theSemaphore->Signal();
+  std::cout <<"I ";
+  std::cout << "must ";
+  std::cout << "print ";
+  std::cout << "first"<<std::endl;
+}
+/*! displays a message second*/
+void taskTwo(std::shared_ptr<Semaphore> theSemaphore){
+  theSemaphore->Wait();
+  std::cout <<"This ";
+  std::cout << "will ";
+  std::cout << "appear ";
+  std::cout << "second"<<std::endl;
+}
+
+
+int main(void){
+  std::thread threadOne, threadTwo;
+  std::shared_ptr<Semaphore> sem( new Semaphore);
+  /**< Launch the threads  */
+  int taskOneDelay=5;
+  threadOne=std::thread(taskTwo,sem);
+  threadTwo=std::thread(taskOne,sem,taskOneDelay);
+  std::cout << "Launched from the main\n";
+   /**< Wait for the threads to finish */
+  threadOne.join();
+  threadTwo.join();
+  return 0;
+}
